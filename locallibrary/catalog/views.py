e7aec95c971987a2ace95f59c61cd45dc3407498
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-23 17:57:04 -0300

    commit

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Author, BookUnstance
+from .models import Book, Author, BookUnstance, Genre, Language
 
 def index(request):
-  
   context = {
     
   }
