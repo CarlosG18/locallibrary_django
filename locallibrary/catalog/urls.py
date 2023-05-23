ef5534fa2282cff00eaca6a4296430f9c7675cf2
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-23 16:02:14 -0300

    init part5

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = "catalog"
 urlpatterns = [
   path("", views.index, name="index"),
+  path("books/", views.books, name="books"),
+  path("authors/", views.authors, name="authors"),
+  path("book/<int:id>/", views.book, name="book"),
+  path("author/<int:id>/", views.author, name="author"),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookUnstance
 
 def index(request):
-  return HttpResponse("teste")
+  
+  context = {
+    
+  }
+  return render(request, "catalog/index.html", context=context)
+  
+def books(request):
+  return HttpResponse("books")
+  
+def authors(request):
+  return HttpResponse("authors")
+  
+def book(request, id):
+  return HttpResponse(f'book id = {id}')
+
+def author(request, id):
+  return HttpResponse(f'authors id =  {id}')
